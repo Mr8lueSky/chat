6f83ed01acea4246f533c91d7339d581b70bc610
Mr8lueSky <73619999+Mr8lueSky@users.noreply.github.com>
2020-10-29 05:38:14 +0300

    Update settings.py

--- chat/settings.py
+++ chat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q90_@2)f!62_et(w8@q@#hps4^i7n-56hua$oi^%vw6vfk=3e&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
